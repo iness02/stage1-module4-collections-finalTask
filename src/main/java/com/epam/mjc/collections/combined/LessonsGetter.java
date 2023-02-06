97d2d960ec77f6d4038cf1626c2bdf7590d696c3
inesa_hakobyan <inesahakobyan18@gmail.com>
2023-02-06 22:13:00 +0400

    init commit2

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -6,8 +6,8 @@ public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
 
         Set<String> lessonsSet = new HashSet<String>();
-        for(Map.Entry<String, List<String>> entry:timetable.entrySet()){
-            for(String lesson:entry.getValue()){
+        for (Map.Entry<String, List<String>> entry : timetable.entrySet()) {
+            for (String lesson : entry.getValue()) {
                 lessonsSet.add(lesson);
             }
         }
